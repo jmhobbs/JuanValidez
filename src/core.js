96457432ec85c9765bfeba4fd29a8b917b12d7d7
John Hobbs <john@velvetcache.org>
2011-08-03 16:59:09 -0500

    Added some method docs to core.

--- src/core.js
+++ src/core.js
@@ -2,6 +2,11 @@ var JuanValidez = {
 	
 	_validators: {},
 
+	/*!
+		Get the names of all currently loaded validators.
+
+		\returns Array
+	*/
 	validators: function () {
 		var key,
 		    keys = [];
@@ -13,16 +18,36 @@ var JuanValidez = {
 		return keys;
 	},
 
+	/*!
+		Register a validator.
+
+		\param name Name of the validator. Be unique but concise.
+		\param fn The validation function.
+	*/
 	addValidator: function ( name, fn ) {
 		this._validators[name] = fn;
 	},
 
-	removeValidator: function ( validator ) {
-		if( this._validators[validator] ) {
-			delete this._validators[validator];
+	/*
+		Remove a registered validator.
+
+		\param name The name of the validator to remove.
+	*/
+	removeValidator: function ( name ) {
+		if( this._validators[name] ) {
+			delete this._validators[name];
 		}
 	},
 
+	/*
+		Execute a single validator. If the validator does not exist, the validation is false.
+
+		\param validator The name of the validator to run.
+		\param value The value to test.
+		\param args Arguments to pass to the validator, as an array. (optional)
+
+		\returns boolean True if the value validated. False if not.
+	*/
 	runValidator: function ( validator, value, args ) {
 		// args is optional
 		args = ( "undefined" == typeof( args ) ) ? [] : args; 
@@ -36,6 +61,16 @@ var JuanValidez = {
 		}
 	},
 
+	/*!
+		Validate a value given a JuanValidez formatted string.
+
+		Example:
+			// Validate the string "hello" against the "required" validator and the "length" validator (minimum length 4, maximum 10)
+			JuanValidez.validate( 'hello', 'required length:4,10' );
+
+		\param value The value to validate.
+		\param validators The string of validators to test against.
+	*/
 	validate: function ( value, validators ) {
 	
 		// If there are no validation rules, it's valid.
